b56972a40e97cb8607b3edf9f66460cbc201fd3e
Aarushi c Pandey <aaruship2002@gmail.com>
2024-10-17 14:51:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // Import Tailwind CSS
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Optional: Start measuring performance, useful if you plan to integrate analytics later.
+// You can uncomment the line below and log performance metrics to the console or send to an endpoint
+// reportWebVitals(console.log);
+
