f85e7834357ef5a5d301a0626db3d07ec557de91
Prince <princekashishbbu@gmail.com>
2024-10-23 21:35:55 +0530

    change

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -1,28 +1,48 @@
-import { ArrowRight } from 'lucide-react';
-import React from 'react';
-
+import { useGSAP } from "@gsap/react";
+import { IconArrowRight } from "@tabler/icons-react";
+import gsap from "gsap";
+import React from "react";
 
 const Home: React.FC = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      "#hbtn",
+      { y: 0 },
+      {
+        y: -10,
+        duration: 0.55,
+        yoyo: true,
+        repeat: -1,
+        ease: "power1.inOut",
+      }
+    );
+  }, []);
   return (
     <div>
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Challenge Your Tech Knowledge with TechHex
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Engage in thrilling one-on-one battles on a hexagonal grid. Test your knowledge of trending tech topics and climb the leaderboard!
-                </p>
-              </div>
-              <div className="space-x-4">
-                <button>Start Playing <ArrowRight className="ml-2 h-4 w-4" /></button>
-              </div>
+      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center items-center">
+        <div className="container px-4 md:px-6 ">
+          <div className=" space-y-4 text-center flex flex-col justify-center items-center gap-5">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                Challenge Your Tech Knowledge with TechHex
+              </h1>
+              <p className="mx-auto max-w-[700px] pt-5 text-gray-500 md:text-xl dark:text-gray-400">
+                Engage in thrilling one-on-one battles on a hexagonal grid. Test
+                your knowledge of trending tech topics and climb the
+                leaderboard!
+              </p>
             </div>
+
+            <button
+              id="hbtn"
+              className="bg-black text-base    text-white flex justify-center items-center px-12 py-3 rounded-xl"
+            >
+              Start Playing <IconArrowRight className="ml-2 h-4 w-4" />
+            </button>
           </div>
-        </section>
+        </div>
+      </section>
     </div>
-  )
+  );
 };
 export default Home;
